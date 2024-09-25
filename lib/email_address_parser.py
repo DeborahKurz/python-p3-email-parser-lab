58bf5758f17fa192a081eea67b7e1a1e2c998058
Deborah Kurz <DeborahKurz6@gmail.com>
2024-09-25 12:13:33 -0600

    Complete Lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
-# your code goes here!
+# Write a program that takes in a string of email addresses, and parses the string into an array.
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+    def parse(self):
+        emails = re.split(r'[\s,]+', self.email_addresses)
+
+        email_pattern= r'[\w\.-]+@[\.\w]'
+
+        valid_emails = {email for email in emails if re.match(email_pattern, email)}
+
+        return sorted(valid_emails)
+        
